25a6ecc4246190e54a189a6f5423c1dada846e81
Prakash K Sinha <prakashksinha@gmail.com>
2014-03-26 18:46:02 -0400

    more dynamic view

--- helitutorial/helitutorial/ViewController.h
+++ helitutorial/helitutorial/ViewController.h
@@ -10,6 +10,7 @@
 
 int Y;
 BOOL Start;
+int RandomPosition;
 
 @interface ViewController : UIViewController
 {
@@ -19,7 +20,26 @@ BOOL Start;
     
     IBOutlet UIImageView *Heli;
     NSTimer *timer;
-        
+    
+    IBOutlet UIImageView *Obstacle1;
+    IBOutlet UIImageView *Obstacle2;
+    
+    IBOutlet UIImageView *Top1;
+    IBOutlet UIImageView *Top2;
+    IBOutlet UIImageView *Top3;
+    IBOutlet UIImageView *Top4;
+    IBOutlet UIImageView *Top5;
+    IBOutlet UIImageView *Top6;
+    IBOutlet UIImageView *Top7;
+    
+    IBOutlet UIImageView *Bottom1;
+    IBOutlet UIImageView *Bottom2;
+    IBOutlet UIImageView *Bottom3;
+    IBOutlet UIImageView *Bottom4;
+    IBOutlet UIImageView *Bottom5;
+    IBOutlet UIImageView *Bottom6;
+    IBOutlet UIImageView *Bottom7;
+    
 }
 
 - (void)HeliMove;
