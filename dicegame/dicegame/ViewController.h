2c13f9df0aca1ec48ea09cd573d12d7b8f692936
Prakash K Sinha <prakashksinha@gmail.com>
2014-03-26 08:43:42 -0400

    Initial Commit

---
+++ dicegame/dicegame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  dicegame
+//
+//  Created by Home | Prakash Sinha on 3/26/14.
+//  Copyright (c) 2014 Home | Prakash Sinha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ dicegame/dicegame/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  dicegame
+//
+//  Created by Home | Prakash Sinha on 3/26/14.
+//  Copyright (c) 2014 Home | Prakash Sinha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
